656c9178befe2bae6a0517c028a1d112bcb4190c
JUAN.JIMENEZCASTRILLO <JUAN.JIMENEZCASTRILLO@ucr.ac.cr>
2026-06-26 14:16:26 -0600

    Creación de Categoria - Juan Carlos Jiménez Castrillo

---
+++ app/Models/Categoria.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $primaryKey = 'idCategoria';
+
+    protected $fillable = [
+        'nombre',
+    ];
+
+    public function materiales()
+    {
+        return $this->hasMany(Material::class, 'idCategoria', 'idCategoria');
+    }
+}
